0ae23e182a3b3c50b96cfed51167305ae177b412
Tony <antony.branquinho@gmail.com>
2025-12-10 11:23:56 -0300

    terminei o header

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const menu = document.querySelector('.menu-idiomas');
 const botao = document.querySelector('.botao-abrir');
 
-botao.addEventListener('click', () => {
+botao.addEventListener('click', (e) => {
+    e.stopPropagation();
     menu.classList.toggle('ativo'); 
+});
+
+ // Fechar o dropdown ao clicar fora dele
+document.addEventListener('click', () => {
+    menu.classList.remove('ativo');           
 });
